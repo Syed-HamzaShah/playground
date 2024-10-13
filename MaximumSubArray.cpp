5e0d02705e33ed02fb5294c2d5eac1c4c0cbd4ce
Syed Hamza Shah <hsworkshop7@gmail.com>
2024-10-13 14:41:54 +0500

    2.41

--- MaximumSubArray.cpp
+++ MaximumSubArray.cpp
@@ -2,20 +2,22 @@
 using namespace std;
 
 int main(){
-    int array[ ] = { 1 , 3  , 4 , 6 , 7 , 9 };
+    int array[ ] = { 3 , -4  , 5 , 4 , -1 , 7 , -8 };
     int size = sizeof(array) / sizeof(int);
+
+    int maxSum = INT_MIN;
 
     for (int i = 0 ; i<size ;i++)
     {
+        int currSum = 0;
         for(int j = i ; j<size ; j++)
         {
-            for (int k = i; k < size; k++)
-            {
-                cout<<array[k]<<' '<<endl;    
-            }
+            currSum += array[j];
+            maxSum = max(maxSum,currSum);
         }
-            cout<<endl;
     }
+
+    cout<<"Max Subarray sum = "<<maxSum<<endl;
 
 
     return 0;
---
+++ TwoSum.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int array[] = { 2 , 7 , 11 , 15};
+    int size = sizeof(array)/sizeof(array[0]);
+    int target = 17;
+   
+    for(int i = 0 ; i < size ; i++)
+    {
+        for(int j = i ; j < size ; j++ )
+        {
+            if(array[i]+array[j] == target)
+            {
+                cout<<i<<" "<<j<<endl;
+            }
+        }
+    }
+
+    return 0;
+}
