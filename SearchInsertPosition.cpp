787390876d8dabfdb2f90d6d6de46f011829c485
Syed Hamza Shah <hsworkshop7@gmail.com>
2024-10-23 19:09:35 +0500

    7.09

---
+++ SearchInsertPosition.cpp
@@ -0,0 +1,31 @@
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    int searchInsert(vector<int> &nums, int target)
+    {
+        int start = 0;
+        int end = nums.size() - 1;
+        int ans = -1;
+
+        while (start <= end)
+        {
+            int mid = start + (end - start) / 2;
+
+            if (nums[mid] == target)
+                return mid;
+
+            if (nums[mid] > target)
+                end = mid - 1;
+
+            if (nums[mid] < target)
+                start = mid + 1;
+
+            if (nums[mid] != target)
+                ans = start;
+        }
+        return ans;
+    }
+};
