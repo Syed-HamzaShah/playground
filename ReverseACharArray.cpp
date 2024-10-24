87d508ab5d691e8dd84d86f243d5fd1887123f6a
Syed Hamza Shah <hsworkshop7@gmail.com>
2024-10-24 15:27:16 +0500

    3.27

---
+++ ReverseACharArray.cpp
@@ -0,0 +1,16 @@
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int start = 0;
+        int end = s.size() - 1;
+
+        while(start < end){
+            swap(s[start] , s[end]);
+            start ++;
+            end --;
+        }
+        
+    }
+};
