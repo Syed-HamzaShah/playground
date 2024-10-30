fa7f4fcc9c6f04440568884aa26799277f5cb537
Syed Hamza Shah <hsworkshop7@gmail.com>
2024-10-30 20:13:58 +0500

    8.13

---
+++ StringCompression.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int compress(vector<char>& chars) {
+        int size = chars.size();
+        int index = 0;
+
+        for(int i = 0 ; i < size ; i++ ){
+            char ch = chars[i];
+            int count = 0;
+
+            while(i < size && chars[i] == ch){
+                count++;
+                i++;
+            }
+
+            if(count == 1){
+                chars[index] = ch;
+                index++;
+            }
+            else{
+                chars[index] = ch;
+                index++;
+                string str = to_string(count);
+                for(char digit : str){
+                    chars[index] = digit;
+                    index++;
+                }
+            }
+            i--;
+        }
+        chars.resize(index);
+        return index;
+    }
+};
