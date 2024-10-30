aed7bdae41d21a1347b0e7bcdcfbbed742273629
Syed Hamza Shah <hsworkshop7@gmail.com>
2024-10-30 20:15:14 +0500

    8.15

--- StringCompression.cpp
+++ StringCompression.cpp
@@ -1,27 +1,38 @@
-class Solution {
+#include <string>
+#include <vector>
+using namespace std;
+
+class Solution
+{
 public:
-    int compress(vector<char>& chars) {
+    int compress(vector<char> &chars)
+    {
         int size = chars.size();
         int index = 0;
 
-        for(int i = 0 ; i < size ; i++ ){
+        for (int i = 0; i < size; i++)
+        {
             char ch = chars[i];
             int count = 0;
 
-            while(i < size && chars[i] == ch){
+            while (i < size && chars[i] == ch)
+            {
                 count++;
                 i++;
             }
 
-            if(count == 1){
+            if (count == 1)
+            {
                 chars[index] = ch;
                 index++;
             }
-            else{
+            else
+            {
                 chars[index] = ch;
                 index++;
                 string str = to_string(count);
-                for(char digit : str){
+                for (char digit : str)
+                {
                     chars[index] = digit;
                     index++;
                 }
