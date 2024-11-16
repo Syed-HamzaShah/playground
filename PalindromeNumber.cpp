c17b2da9968c1f91a7efc8f2a1401f0278f01204
Syed Hamza Shah <hsworkshop7@gmail.com>
2024-11-16 12:20:38 +0500

    12.20

---
+++ PalindromeNumber.cpp
@@ -0,0 +1,21 @@
+#include <string>
+using namespace std;
+class Solution
+{
+public:
+    bool isPalindrome(int x)
+    {
+        string s = to_string(x);
+        int left = 0, right = s.length() - 1;
+        while (left <= right)
+        {
+            if (s[left] != s[right])
+            {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+};
