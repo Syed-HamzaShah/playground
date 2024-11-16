bbc6eff675ee3ca821a7f26bfe02aaa6b22c83c6
Syed Hamza Shah <hsworkshop7@gmail.com>
2024-11-16 11:37:39 +0500

    11.37

---
+++ MissingNumbers.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    int missingNumber(vector<int> &nums)
+    {
+        int n = nums.size();
+        int actualSum = 0;
+        int expectedSum = n * (n + 1) / 2;
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            actualSum += nums[i];
+        }
+
+        return expectedSum - actualSum;
+    }
+};
