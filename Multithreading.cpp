0597a512eb2b7498f6ec69264da9506e6891e348
Syed Hamza Shah <hsworkshop7@gmail.com>
2024-11-21 17:39:29 +0500

    5.39

---
+++ Multithreading.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+
+using namespace std;
+int a,b;
+
+
+mutex mux;
+
+void add()
+{
+    lock_guard<mutex> lock(mux);
+    cout << a + b << endl;
+}
+
+void sub()
+{
+    lock_guard<mutex> lock(mux);
+    cout << a - b << endl;
+}
+
+void divide()
+{
+    lock_guard<mutex> lock(mux);
+    if (b == 0)
+        cout << "Divide Not possible" << endl;
+    else
+        cout << a / b << endl;
+}
+
+void mult()
+{
+    lock_guard<mutex> lock(mux);
+    cout << a * b << endl;
+}
+int main()
+{
+
+cout<<"Enter two number:";
+cin>>a>>b;
+    thread t1(add);
+    thread t2(sub);
+    thread t3(divide);
+    thread t4(mult);
+    t1.join();
+    t2.join();
+    t3.join();
+    t4.join();
+
+    return 0;
+}
---
+++ mt.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <thread>
+using namespace std;
+
+void task1()
+{
+    cout << "Task 1" << endl;
+}
+void task2()
+{
+    cout << "Task 2" << endl;
+}
+int main()
+{
+    thread t1(task1);
+    thread t2(task2);
+
+    t1.join();
+    t2.join();
+
+    cout<<"t1 and t2 are successfully simulated"<<endl;
+    return 0;
+}
