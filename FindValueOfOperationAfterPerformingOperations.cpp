db149e275f76dcfce5e22443cccaaa548fd69aa1
Syed Hamza Shah <hsworkshop7@gmail.com>
2024-11-28 19:50:04 +0500

    7.50

---
+++ FindValueOfOperationAfterPerformingOperations.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+#include <string>
+using namespace std;
+
+class Solution
+{
+public:
+    int finalValueAfterOperations(vector<string> &operations)
+    {
+        int ans = 0;
+
+        for (int i = 0; i < operations.size(); i++)
+        {
+            if (operations[i] == "++X" || operations[i] == "X++")
+                ans++;
+            else
+                ans--;
+        }
+        return ans;
+    }
+};
