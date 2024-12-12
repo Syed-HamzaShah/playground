255c16f3982bb60feec8a878e39284c29766bdb8
Syed Hamza Shah <hsworkshop7@gmail.com>
2024-12-12 18:26:30 +0500

    6.26

--- LinkedListFromScratch.cpp
+++ LinkedListFromScratch.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
 using namespace std;
 
+class Node
+{
+    int data;
+    int *link = NULL;
+};
+
+class LinkedList
+{
+private:
+    Node *head;
+
+    Node add(int i, int *j)
+    {
+        int *curr;
+    }
+};
+
 int main()
 {
 
