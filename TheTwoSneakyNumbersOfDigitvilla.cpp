7282494a366bd9253f928252c2c21351f7082f5c
Syed Hamza Shah <hsworkshop7@gmail.com>
2024-12-14 22:06:19 +0500

    10.06

---
+++ TheTwoSneakyNumbersOfDigitvilla.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> getSneakyNumbers(vector<int> &nums)
+    {
+        vector<int> ans;
+        sort(nums.begin(), nums.end());
+
+        for (int i = 1; i < nums.size(); i++)
+        {
+            if (nums[i] == nums[i - 1])
+                ans.push_back(nums[i]);
+        }
+
+        return ans;
+    }
+};
