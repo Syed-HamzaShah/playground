9b290fc7239557271bc6a12ef398063986f71274
Syed Hamza Shah <hsworkshop7@gmail.com>
2024-12-19 19:00:54 +0500

    7.00

---
+++ SquaresOfASortedArray.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> sortedSquares(vector<int> &nums)
+    {
+        vector<int> ans;
+
+        for (int num : nums)
+        {
+            ans.push_back(abs(num * num));
+        }
+
+        sort(ans.begin(), ans.end());
+        return ans;
+    }
+};
