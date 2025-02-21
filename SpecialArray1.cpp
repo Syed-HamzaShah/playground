7920c8d87b7986c188efa8bea24870f38f629fea
Syed Hamza Shah <hsworkshop7@gmail.com>
2025-02-21 19:30:16 +0500

    7.30

---
+++ SpecialArray1.cpp
@@ -0,0 +1,16 @@
+class Solution
+{
+public:
+    bool isArraySpecial(vector<int> &nums)
+    {
+        for (int i = 1; i < nums.size(); i++)
+        {
+            if (nums[i] % 2 == 0 && nums[i - 1] % 2 != 0)
+                continue;
+            if (nums[i] % 2 != 0 && nums[i - 1] % 2 == 0)
+                continue;
+            return false;
+        }
+        return true;
+    }
+};
