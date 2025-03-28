7083b758c6e8be1eecf530173d6de744852d15f1
Syed Hamza Shah <hsworkshop7@gmail.com>
2025-03-28 18:54:28 +0500

    6.54

---
+++ FriendRequest2WhoHasTheMostFriends.sql
@@ -0,0 +1,14 @@
+WITH FriendCounts AS (
+    SELECT requester_id AS id, COUNT(*) AS num
+    FROM RequestAccepted
+    GROUP BY requester_id
+    UNION ALL
+    SELECT accepter_id AS id, COUNT(*) AS num
+    FROM RequestAccepted
+    GROUP BY accepter_id
+)
+SELECT id, SUM(num) AS num
+FROM FriendCounts
+GROUP BY id
+ORDER BY num DESC
+LIMIT 1;
