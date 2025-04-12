e8b643306000279bf4f857047a476eeaa640a084
Syed Hamza Shah <hsworkshop7@gmail.com>
2025-04-12 15:45:42 +0500

    3.45

---
+++ Node.js
@@ -0,0 +1,16 @@
+const amount = 12;
+
+if (amount >= 10) console.log("Large number");
+else console.log("Small number");
+
+console.log("Hello World!");
+
+console.log(__dirname);
+console.log(__filename);
+console.log(require);
+console.log(module);
+console.log(process);
+
+setInterval(() => {
+  console.log("Hello World!");
+}, 1000);
