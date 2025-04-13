5647253a3db25555f573c2325e5bf3a9e976f053
Syed Hamza Shah <hsworkshop7@gmail.com>
2025-04-13 09:40:47 +0500

    9.40

--- Node.js
+++ Node.js
@@ -1,16 +1,22 @@
-const amount = 12;
+// const amount = 12;
 
-if (amount >= 10) console.log("Large number");
-else console.log("Small number");
+// if (amount >= 10) console.log("Large number");
+// else console.log("Small number");
 
-console.log("Hello World!");
+// console.log("Hello World!");
 
-console.log(__dirname);
-console.log(__filename);
-console.log(require);
-console.log(module);
-console.log(process);
+// console.log(__dirname);
+// console.log(__filename);
+// console.log(require);
+// console.log(module);
+// console.log(process);
 
-setInterval(() => {
-  console.log("Hello World!");
-}, 1000);
+// setInterval(() => {
+//   console.log("Hello World!");
+// }, 1000);
+
+const names = require("./NodeNames.js");
+const SayHi = require("./NodeFunction.js");
+
+SayHi("Saad");
+SayHi("Ali");
---
+++ NodeNames.js
@@ -0,0 +1,6 @@
+const hamza = "Hamza";
+
+const saad = "Saad";
+const ali = "Ali";
+
+module.export = { saad, ali };
