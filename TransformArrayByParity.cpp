c4d3257686843ed3a6fcf033100028e6fe68a307
Syed Hamza Shah <hsworkshop7@gmail.com>
2025-04-21 20:21:41 +0500

    8.21

---
+++ TransformArrayByParity.cpp
@@ -0,0 +1,40 @@
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> transformArray(vector<int> &nums)
+    {
+        int i = 0;
+        int j = nums.size() - 1;
+
+        while (i <= j)
+        {
+            if (nums[i] % 2 == 0 && nums[j] % 2 == 1)
+            { // I - Even, J - Odd
+                nums[i] = 0;
+                nums[j] = 1;
+                i++, j--;
+            }
+            else if (nums[i] % 2 == 0 && nums[j] % 2 == 0)
+            { // I - Even , J - Even
+                nums[i] = 0;
+                i++;
+            }
+            else if (nums[i] % 2 == 1 && nums[j] % 2 == 1)
+            { // I - Odd, J - Odd
+                nums[j] = 1;
+                j--;
+            }
+            else
+            {
+                nums[i] = 0;
+                nums[j] = 1;
+                i++, j--;
+            }
+        }
+
+        return nums;
+    }
+};
