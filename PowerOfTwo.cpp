2352674676c5d6a0ec569cd1d8beebce97c5a773
Syed Hamza Shah <hsworkshop7@gmail.com>
2025-05-06 21:27:34 +0500

    9.27

--- PowerOfTwo.cpp
+++ PowerOfTwo.cpp
@@ -1,26 +1,40 @@
 #include <unordered_set>
 using namespace std;
 
+// class Solution
+// {
+// public:
+//     bool isPowerOfTwo(int n)
+//     {
+//         if (n <= 0)
+//             return false;
+
+//         unordered_set<int> s;
+
+//         while (n != 0)
+//         {
+//             int num = n % 2;
+//             if (s.find(1) != s.end())
+//                 return false;
+
+//             n /= 2;
+//             s.insert(num);
+//         }
+
+//         return true;
+//     }
+// };
+
 class Solution
 {
 public:
     bool isPowerOfTwo(int n)
     {
-        if (n <= 0)
+        if (n == 1)
+            return true;
+        if (n <= 0 || n % 2 != 0)
             return false;
 
-        unordered_set<int> s;
-
-        while (n != 0)
-        {
-            int num = n % 2;
-            if (s.find(1) != s.end())
-                return false;
-
-            n /= 2;
-            s.insert(num);
-        }
-
-        return true;
+        return isPowerOfTwo(n / 2);
     }
 };
