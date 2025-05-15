17e9bfb30dd15671c798f8e1bff8d1700762ea22
Syed Hamza Shah <hsworkshop7@gmail.com>
2025-05-15 20:07:34 +0500

    8.07

---
+++ MaxNumberOfCoinsYouCanGet.cpp
@@ -0,0 +1,19 @@
+class Solution
+{
+public:
+    int maxCoins(vector<int> &piles)
+    {
+        sort(piles.begin(), piles.end());
+        int i = 0;
+        int j = piles.size() - 2;
+        int count = 0;
+
+        while (i <= j)
+        {
+            count += piles[j];
+            i++;
+            j -= 2;
+        }
+        return count;
+    }
+};
