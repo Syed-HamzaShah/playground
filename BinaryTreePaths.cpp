9e60d38017edd7718b36254bbf2ef186bcbcd9d4
Syed Hamza Shah <hsworkshop7@gmail.com>
2025-08-11 18:26:12 +0500

    BinaryTreePaths

---
+++ BinaryTreePaths.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    vector<string> ans;
+public:
+    void traverse(TreeNode* root, string s){
+        if(!root->left && !root->right){
+            s += to_string(root->val);
+            ans.push_back(s);
+            return;
+        }
+        s += to_string(root->val);
+        s += "->";
+        if(root->left) traverse(root->left,s);
+        if(root->right) traverse(root->right,s);
+
+    }
+
+    vector<string> binaryTreePaths(TreeNode* root) {
+        if(!root) return ans;
+        traverse(root,"");
+        return ans;
+    }
+};
