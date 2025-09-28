9bf725f1d8ccab786e941554eb6bf269ac6b72bc
Syed Hamza Shah <hsworkshop7@gmail.com>
2025-09-28 14:08:38 +0500

    LargestParameterTriangle

---
+++ LargestParameterTriangle.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int largestPerimeter(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+
+        for(int i = nums.size() - 2 ; i > 0; i--)
+            if(nums[i] + nums[i-1] > nums[i+1]) 
+                return nums[i-1] + nums[i] + nums[i+1];
+        
+        return 0;
+    }
+};
