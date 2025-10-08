c61fae8576ccc349221103533d9df33234649efb
Syed Hamza Shah <hsworkshop7@gmail.com>
2025-10-08 12:46:23 +0500

    AverageOfLevelsInBinaryTree

---
+++ AverageOfLevelsInBinaryTree.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<double> averageOfLevels(TreeNode* root) {
+        if(!root) return {};
+        vector<double> ans;
+        queue<TreeNode*> q;
+        q.push(root);
+        
+        while(!q.empty()){
+            int size = q.size();
+            double temp = 0;
+
+            for(int i = 0; i < size; i++){
+                TreeNode* t = q.front();
+                q.pop();
+
+                temp += t->val;
+
+                if(t->left)q.push(t->left);
+                if(t->right)q.push(t->right);
+            }
+
+            temp /= size;
+            ans.push_back(temp);
+        }
+
+        return ans;
+    }
+};
