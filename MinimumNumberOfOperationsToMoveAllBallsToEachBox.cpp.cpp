7da1bdf0cfa5380228520518804962c2c4b7e821
Syed Hamza Shah <hsworkshop7@gmail.com>
2025-10-13 18:27:43 +0500

    MinimumNumberOfOperationsToMoveAllBallsToEachBox.cpp

---
+++ MinimumNumberOfOperationsToMoveAllBallsToEachBox.cpp.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> minOperations(string boxes) {
+        vector<int> ans;
+        for(int i = 0; i < boxes.length(); i++){
+            int temp = 0;
+            for(int j = 0; j < boxes.length(); j++){
+                if(i == j) continue;
+                if(boxes[j] == '1') temp += abs(i - j);
+            }
+            ans.push_back(temp);
+        }
+        return ans;
+    }
+};
