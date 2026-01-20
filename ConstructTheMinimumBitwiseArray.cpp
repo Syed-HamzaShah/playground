1e51c29c601cf9f503b5e6ea0524a3da187cee2c
Syed Hamza Shah <hsworkshop7@gmail.com>
2026-01-20 14:10:51 +0500

    ConstructTheMinimumBitwiseArray

---
+++ ConstructTheMinimumBitwiseArray.cpp
@@ -0,0 +1,13 @@
+for (int i = 0; i < nums.size(); i++) {
+            int tmp = nums[i];
+            for (int j = 1; j < nums[i]; j++) {
+                if ((j | (j + 1)) == nums[i]) {
+                    nums[i] = j;
+                    break;
+                }
+            }
+            if (nums[i] == tmp) {
+                nums[i] = -1;
+            }
+        }
+        return nums;
