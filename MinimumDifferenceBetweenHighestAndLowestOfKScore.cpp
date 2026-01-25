4aa39550145bba7460795ca972c7a49dc6b7e7cf
Syed Hamza Shah <hsworkshop7@gmail.com>
2026-01-25 15:44:21 +0500

    MinimumDifferenceBetweenHighestAndLowestOfKScore

---
+++ MinimumDifferenceBetweenHighestAndLowestOfKScore.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minimumDifference(vector<int>& nums, int k) {
+        if (k == 1) return 0;
+        int ans = INT_MAX;
+
+        sort(nums.begin(), nums.end());
+
+        for (int i = 0; i + k - 1 < nums.size(); i++) 
+            ans = min(ans, nums[i + k - 1] - nums[i]);
+
+        return ans;
+    }
+};
