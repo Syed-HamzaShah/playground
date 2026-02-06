786f64770f5c1ef9a0d3e643b484b3e4c3b0e354
Syed Hamza Shah <hsworkshop7@gmail.com>
2026-02-06 18:57:06 +0500

    MinimumRemovalToBalanceArray

---
+++ MinimumRemovalToBalanceArray.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int minRemoval(vector<int>& nums, int k) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+
+        int i = 0;
+        int maxWindow = 0;
+
+        for (int j = 0; j < n; j++) {
+            while ((long long)nums[j] > (long long)k * nums[i]) {
+                i++;
+            }
+            maxWindow = max(maxWindow, j - i + 1);
+        }
+
+        return n - maxWindow;
+    }
+};
