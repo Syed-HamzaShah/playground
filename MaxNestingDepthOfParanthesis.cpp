7ff148bcde5b8cc3d4b9fe8ebe9b0f96da2929e4
Syed Hamza Shah <hsworkshop7@gmail.com>
2026-03-12 18:55:34 +0500

    MaximumNestingDepthOfParnthesis

---
+++ MaxNestingDepthOfParanthesis.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxDepth(string s) {
+        int depth = 0, ans = 0;
+
+        for(char c : s){
+            if(c == '('){
+                depth++;
+                ans = max(ans, depth);
+            }
+            else if(c == ')')
+                depth--;
+        }
+
+        return ans;
+    }
+};
