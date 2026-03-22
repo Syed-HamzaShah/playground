8c58edb657c882d296a2f191287272ebd288831d
Syed Hamza Shah <hsworkshop7@gmail.com>
2026-03-22 17:18:35 +0500

    DetermineWeatherMatrixCanBeObtainedByRotation

---
+++ DetermineWeatherMatrixCanBeObtainedByRotation.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int size = matrix.size();
+
+        for(int n = 0; n < size; n++)
+            for(int i = n , j = n; i < size && j < size; i++ , j++)
+                swap(matrix[n][i], matrix[j][n]);
+            
+        for(int n = 0; n < size; n++)
+            for(int i = 0 , j = size - 1; i <= j && j >= i; i++ , j--)
+                swap(matrix[n][i], matrix[n][j]);
+        
+    }
+
+    bool findRotation(vector<vector<int>>& mat, vector<vector<int>>& target) {
+        if(mat == target) return true;
+        
+        for(int i = 0; i < 3; i++){
+            rotate(mat);
+            if(mat == target)
+                return true;
+        }
+        return false;
+    }
+};
