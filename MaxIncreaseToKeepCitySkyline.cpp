939a657ce09ae6cff56ed845d1fe0cc6360e69fb
Syed Hamza Shah <hsworkshop7@gmail.com>
2026-04-27 12:10:40 +0500

    MaxIncreaseToKeepCitySkyline

---
+++ MaxIncreaseToKeepCitySkyline.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxIncreaseKeepingSkyline(vector<vector<int>>& grid) {
+        int n = grid.size();
+        vector<int> row(n, INT_MIN);
+        vector<int> col(n, INT_MIN);
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                row[i] = max(row[i], grid[i][j]);
+                col[j] = max(col[j], grid[i][j]);
+            }
+        }
+
+        int ans = 0;
+
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < n; j++)
+                ans += min(row[i], col[j]) - grid[i][j];
+
+        return ans;
+    }
+};
