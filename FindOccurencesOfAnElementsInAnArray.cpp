1cfcdf221aaa921c698cca636ee6dba71aab2fe1
Syed Hamza Shah <hsworkshop7@gmail.com>
2026-05-10 07:29:30 +0500

    FindOccurencesOfAnElementsInAnArray

---
+++ FindOccurencesOfAnElementsInAnArray.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> occurrencesOfElement(vector<int>& nums, vector<int>& queries, int x) {
+        unordered_map<int, int> mp;
+        int count = 0; 
+        int n = nums.size(); 
+
+        for(int i = 0; i < n; i++) {
+            if(nums[i] == x) {
+                count++;
+                mp[count] = i;
+            }
+        }
+
+        for(int i = 0; i < queries.size(); i++)
+            if(mp.find(queries[i]) != mp.end())
+                queries[i] = mp[queries[i]];
+            else
+                queries[i] = -1;
+
+        return queries;
+    }
+};
