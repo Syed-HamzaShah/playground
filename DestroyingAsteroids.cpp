1b8a1fe15abc3c24ded0ffd677d376614a83be72
Syed Hamza Shah <hsworkshop7@gmail.com>
2026-05-31 18:30:46 +0500

    DestroyingAsteroids

---
+++ DestroyingAsteroids.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool asteroidsDestroyed(long long mass, vector<int>& asteroids) {
+        sort(asteroids.begin(), asteroids.end());
+        for(int asteroid: asteroids)
+            if(asteroid <= mass)
+                mass += asteroid;
+            else
+                return false;
+        return true;            
+    }
+};
