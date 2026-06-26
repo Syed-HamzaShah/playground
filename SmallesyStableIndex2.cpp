cc46a17f3c1a177b8fd6b86a9a807df8cf1b22b2
Syed Hamza Shah <hsworkshop7@gmail.com>
2026-06-26 17:35:15 +0500

    SmallesyStableIndex2

---
+++ SmallesyStableIndex2.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int firstStableIndex(vector<int>& nums, int k) {
+        int n=nums.size();
+        vector<int> prefixmax(n,INT_MIN);
+        vector<int> suffixmin(n,INT_MAX);
+
+        prefixmax[0]=nums[0];
+        suffixmin[n-1]=nums[n-1];
+
+        for(int i=1;i<n;i++)
+            prefixmax[i]=max(nums[i],prefixmax[i-1]);
+
+        for(int i=n-2;i>=0;i--)
+            suffixmin[i]=min(nums[i],suffixmin[i+1]);
+
+
+        for(int i=0;i<n;i++){
+            int score=prefixmax[i]-suffixmin[i];
+            if(score<=k)
+                return i;
+        }    
+                
+        return -1;
+    }
+};
