4f0a69139f0466989e431ce58a3442994c566b7b
Syed Hamza Shah <hsworkshop7@gmail.com>
2026-07-18 01:47:09 +0500

    DeleteColumnsToMakeSorted

---
+++ DeleteColumnsToMakeSorted.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minDeletionSize(vector<string>& strs) {
+        int ans = 0;
+
+        for(int i = 0; i < strs[0].size(); i++){
+            bool isSorted = true;
+            for(int j = 0; j < strs.size() - 1; j++){
+                if(strs[j][i] > strs[j+1][i]){
+                    isSorted = false;
+                    break;
+                }
+            }
+            if(!isSorted) ans++;
+        }
+
+        return ans;
+    }
+};
