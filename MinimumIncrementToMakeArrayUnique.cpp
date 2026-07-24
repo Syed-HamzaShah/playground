6799d4115148a874869b43f08df582e5eed2455c
Syed Hamza Shah <hsworkshop7@gmail.com>
2026-07-24 18:24:27 +0500

    MinimumIncrementToMakeArrayUnique

---
+++ MinimumIncrementToMakeArrayUnique.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minIncrementForUnique(vector<int>& nums) {
+        int ans = 0;
+        sort(nums.begin(), nums.end());
+
+        for(int i = 1; i < nums.size(); i++){
+            if(nums[i] <= nums[i-1]){
+                int temp = nums[i-1] + 1 - nums[i];
+                ans += temp;
+                nums[i] = nums[i-1] + 1;
+            }
+        }
+        
+        return ans;
+    }
+};
