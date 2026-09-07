543986cf0ddc3a1ac261bce717c3043e4e90b0d5
Syed Hamza Shah <hsworkshop7@gmail.com>
2026-09-07 13:15:05 +0500

    CountValidPrefixes

---
+++ CountValidPrefixes.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int countValidPrefixes(string s) {
+        int one = 0;
+        int zero = 0;
+        int ans = 0;
+
+        for(int i = 0; i < s.length(); i++){
+            if(s[i] == '0') zero++;
+            else one++;
+
+            if(abs(zero - one) <= 1) ans++;
+        }
+        return ans;
+    }
+};
