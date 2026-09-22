5a512155494cd0c6c612cc246da9bdf50b578318
Syed Hamza Shah <hsworkshop7@gmail.com>
2026-09-22 13:37:49 +0500

    MaximumDifferneceBetweenAdjacentElementsInACircularArray

---
+++ MaximumDifferneceBetweenAdjacentElementsInACircularArray.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxAdjacentDistance(vector<int>& nums) {
+        if(nums.size() == 0) return 0;
+        int n = nums.size();
+        int diff = abs(nums[0] - nums[n - 1]);
+
+        for(int i = 0; i < n -1; i++)
+            diff = max(diff, abs(nums[i] - nums[i + 1]));
+        
+        return diff;
+    }
+};
